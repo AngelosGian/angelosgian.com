97cada995de7eef58a96942a0611bde9d63eac00
AngelosGian <angelos.giannourakos@gmail.com>
2022-11-25 23:49:46 +0200

    adding css

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const env = require('dotenv')
+const routes = express.Router()
 env.config({path: './config/.env'})
 
 
+//General Setting 
+app.set('view engine', 'ejs')
+app.use('/public', express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-const port = process.env.PORT || 3000;
+// Routes
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
 
-app.listen(process.env.PORT, () => console.log(`It is alive on port ${port}`));
+
+
+app.listen(process.env.PORT, () => {console.log(`It is alive on port ${process.env.PORT}`)});
